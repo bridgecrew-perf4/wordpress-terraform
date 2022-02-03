aac1ccc0a1e0668d7ed6cd1180d0a93cef11c997
Aleksei Fedorov <telesis@mail.ru>
2022-02-03 23:07:16 +0300

    security group id

--- 0-day/ws_deploy.tf
+++ 0-day/ws_deploy.tf
@@ -54,4 +54,8 @@ resource "aws_securitygroup" "wp_sg" {
 outout "instance_id" {
     value = aws_instance.web_server_ec2.id
     }
+
+outout "instance_securitygroup" {
+    value = aws_security_group.web_server_ec2.id
+    }
         
